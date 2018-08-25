5eeec9aa7b36ba5bcdda6a62496676667a4a8f69
Nathan Baylon <nathanbaylon@gmail.com>
2018-08-26 01:00:55 +1000

    Added a person class, allows users to make decisions and changes

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,34 @@
-print("Hello world!")
+# Creating a fruit class
+class Person:
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
 
-number = input("Please enter a number: ")
+        print("My name is {} {} and I have been created!".format(self.name, self.surname))
 
-print("I have {} oranges in my basket.".format(number))
+    def greeting(self):
+        print("Greetings! My name is {} {} and I am {} years old.".format(self.name, self.surname, self.age))
+
+    def changeName(self):
+        self.name = str(input("What is my new name? "))
+        print("My new name now is {} {}!".format(self.name, self.surname))
+
+    def askPerson(self):
+        self.answer = int(input("What would you like to do?\n1. Greet me\n2. Change my name\n"))
+        return self.answer
+
+person1 = Person("Steve", "Rogers", 24)
+
+while True:
+    print("if you want to quit, enter 0")
+    choice = person1.askPerson()
+
+    if choice == 0:
+        break
+    elif choice == 1:
+        person1.greeting()
+    elif choice == 2:
+        person1.changeName()
+    else:
+        print("Sorry try again")
