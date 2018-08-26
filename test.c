a9522b533dc2c04268bba0aaef0f70cd09d0b970
Nathan Baylon <nathanbaylon@gmail.com>
2018-08-27 00:13:02 +1000

    Added comments to test.c

--- test.c
+++ test.c
@@ -2,9 +2,11 @@
 
 int main(int argc, char* argv[]) {
 
+    // Creating an int and a pointer for it
     int digit = 4;
     int *pDigit = &digit;
 
+    // Printing the memory address of the int to the screen using the pointer
     printf("%p\n", pDigit);
 
     return 0;
