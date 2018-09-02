fc357ce394d5053e74c5aa6e4732910a049b05bf
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-02 17:10:13 +1000

    Changed Submit Button and it now appears with a messagebox

--- Tk/CheckButton.py
+++ Tk/CheckButton.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 root = Tk()
 root.geometry("500x500")
 
 name = StringVar()
 
-def changevalue():
-    print("The box is currently on {}!".format(name.get()))
+def submitvalue():
+    messagebox.showinfo("Name Submission", "You have submitted {}".format(name.get()))
 
 # Determines if the user wants to set the Nathan or John 
 enable_video = Checkbutton(root, text="Video", variable=name, onvalue="Nathan", offvalue="John")
@@ -15,7 +16,7 @@ enable_video.pack()
 # enable_video.grid(row=0, column=0)
 
 # Print out the value of number 
-video_button = Button(root, text="Show value", command=changevalue)
+video_button = Button(root, text="Submit", command=submitvalue)
 video_button.pack()
 
 video_label = Label(root, textvariable=name)
