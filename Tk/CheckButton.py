5613e5266cdb2077225b57139a32109d4534976d
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-03 13:31:05 +1000

    Adjusted function/var names to name and added a nil condition

--- Tk/CheckButton.py
+++ Tk/CheckButton.py
@@ -8,18 +8,21 @@ root.geometry("500x500")
 name = StringVar()
 
 def submitvalue():
-    messagebox.showinfo("Name Submission", "You have submitted {}".format(name.get()))
+    if len(name.get()) == 0:
+        messagebox.showinfo("Error", "You have submitted no name.")
+    else:
+        messagebox.showinfo("Name Submission", "You have submitted {}".format(name.get()))
 
 # Determines if the user wants to set the Nathan or John 
-enable_video = Checkbutton(root, text="Video", variable=name, onvalue="Nathan", offvalue="John")
-enable_video.pack()
-# enable_video.grid(row=0, column=0)
+enable_name = Checkbutton(root, text="Name", variable=name, onvalue="Nathan", offvalue="John")
+enable_name.pack()
+# enable_name.grid(row=0, column=0)
 
 # Print out the value of number 
-video_button = Button(root, text="Submit", command=submitvalue)
-video_button.pack()
+name_button = Button(root, text="Submit", command=submitvalue)
+name_button.pack()
 
-video_label = Label(root, textvariable=name)
-video_label.pack()
+name_label = Label(root, textvariable=name)
+name_label.pack()
 
 root.mainloop()
