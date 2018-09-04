62f2bc7a39a3367a1ace3df1ee62df9f66637bf6
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-04 13:28:31 +1000

    Started PasswordGenerator.py project

---
+++ Tk/PasswordGenerator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+
+# Create a window
+root = Tk()
+root.geometry("720x540")
+root.title("Password Generator by Nathan Baylon")
+
+# Run window in mainloop
+root.mainloop()
