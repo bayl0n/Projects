f1ffa2c3d9c808ad0eb0a56898daa51c62980886
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-04 13:37:11 +1000

    Implemented simple GUI

--- Tk/PasswordGenerator.py
+++ Tk/PasswordGenerator.py
@@ -5,7 +5,18 @@ from tkinter import ttk
 # Create a window
 root = Tk()
 root.geometry("720x540")
+root.resizable(False, False)
 root.title("Password Generator by Nathan Baylon")
+
+password_var = "TEXT"
+
+################ INTERFACE ###################
+
+password_label = Label(root, text="Password: ")
+password_label.grid(row=0, column=0)
+
+password_display = Label(root, text=password_var)
+password_display.grid(row=0, column=1)
 
 # Run window in mainloop
 root.mainloop()
