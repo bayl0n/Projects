46ccf24367d3a71a75491750ba31d0b16b02afb5
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-08 01:25:11 +1000

    Added Web Scraping folder, Created OpenBrowser.py

---
+++ WebScraping/OpenBrowser.py
@@ -0,0 +1,5 @@
+# This module comes with python and it opens a webbrowser to a specific page
+import webbrowser
+
+# This should open a browser and go to google.com
+webbrowser.open('http://www.google.com')
