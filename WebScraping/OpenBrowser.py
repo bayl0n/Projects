ae6180c9a187cfa58327d199870bdabca7ab5808
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-10 10:20:50 +1000

    Started using bs4, need to finish

--- WebScraping/OpenBrowser.py
+++ WebScraping/OpenBrowser.py
@@ -1,12 +1,19 @@
 # This module comes with python and it opens a webbrowser to a specific page
 import webbrowser
+import requests
+from bs4 import BeautifulSoup
 
-def main():
-
+def OpenChrome():
+    
     print("Opening Google!")
 
     # This should open a browser and go to google.com
     webbrowser.open('http://www.google.com')
+
+def main():
+    page = requests.get('https://www.macworld.co.uk/feature/mac/best-macbook-2018-3665084/')
+
+    soup = BeautifulSoup(page.text, 'html.parser')
 
 if __name__ == "__main__":
     main()
