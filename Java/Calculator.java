d6c8183ad1c9d8fb0a51d6601409a129c8e14201
Nathan Baylon <nathanbaylon@gmail.com>
2018-09-28 22:42:20 +1000

    Created Calculator.java

---
+++ Java/Calculator.java
@@ -0,0 +1,8 @@
+// The goal for this is to make a text based calculator in Java
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("This is a test!");
+	}
+
+}
