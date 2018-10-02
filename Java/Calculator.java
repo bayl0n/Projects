d83f8b4b092e77323a76ddbd70fc8793293f5894
Nathan Baylon <nathanbaylon@gmail.com>
2018-10-02 22:51:46 +1000

    Testing out conditionals in Java.

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -7,11 +7,23 @@ public class Calculator {
 		System.out.println("Hello " + name + "!");
 	}
 
+	public void compareNumbers(int x, int y) {
+		if(x == y) {
+			System.out.println(x + " is equal to " + y + ".");
+		} else if (x > y) {
+			System.out.println(x + " is bigger than " + y + ".");
+		} else if (x < y) {
+			System.out.println(x + " is less than " + y + ".");
+		}
+	}
+
 	public static void main(String[] args) {
 
 		Calculator app = new Calculator();
 
 		app.sayHello();
+
+		app.compareNumbers(2, 2);
 
 	}
 
