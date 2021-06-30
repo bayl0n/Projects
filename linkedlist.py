a56a054e9554e2fc32d71dd0471ceea6797e1b26
Nathan Baylon <nathanbaylon@gmail.com>
2021-07-01 02:42:02 +1000

    Linkedlist methods

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
 class LinkedList:
     def __init__(self):
-        pass
+        self.head = None
+
+    def insert(self, data):
+        newNode = Node(data)
+
+        temp = self.head
+        newNode.next = temp
+        self.head = newNode
+
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.data)
+            current = current.next
+
+
+if __name__ == "__main__":
+    myList = LinkedList()
+
+    myList.insert("Bob")
+    myList.insert("James")
+    myList.insert("Apple")
+    myList.insert("Private")
+
+    myList.display()
